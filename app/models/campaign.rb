b9ef09947390232b08577e80029a9fbeaccd393f
pedrosmmoreira <pedro@pedrosmmoreira.com>
2013-08-15 01:52:09 +0100

    Add new campaign form

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,12 +1,23 @@
 class Campaign < ActiveRecord::Base
+  include Rails.application.routes.url_helpers
+
   belongs_to :category
   belongs_to :musician
   has_many :backers, through: :reservations, source: :user
   has_many :reservations
 
+  def base_uri
+    campaign_path(self)
+  end
+
   has_attached_file :full_track,
     storage: :s3,
     s3_credentials: S3_CREDENTIALS
+
+  def save
+    super
+    update_campaign_track_metadata
+  end
 
   def is_available_for?(user)
     true unless backers.include? user
@@ -25,7 +36,6 @@ class Campaign < ActiveRecord::Base
   end
 
   def progress
-    (100 * reserved / requested_likes.to_i).round
   end
 
   def reserved
@@ -33,7 +43,18 @@ class Campaign < ActiveRecord::Base
   end
 
   def track_info
-    client = Soundcloud.new(client_id: ENV['SC_CLIENT_ID'])
+    client = Soundcloud.new(client_id: ENV['SC_LOCAL_ID'])
     client.get("/tracks/#{track_id}")
+  end
+
+  def update_campaign_track_metadata
+    client = Soundcloud.new(access_token: musician.soundcloud_account.access_token)
+    track = client.get("/tracks/#{self.track_id}")
+    client.put(track.uri, track: {
+      sharing: 'public',
+      embedabble_by: 'all',
+      downloadable: false,
+      description: "Help launch this track at http://www.headblendr.com/campaigns/#{id}"
+    })
   end
 end
