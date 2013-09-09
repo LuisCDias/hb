c6719e2e836a42ec5aa6be2cf0cb25bd6e974708
pedrosmmoreira <pedro@pedrosmmoreira.com>
2013-09-09 23:37:21 +0100

    Fix campaign created mailer

--- app/models/campaign_created_notification.rb
+++ app/models/campaign_created_notification.rb
@@ -1,6 +1,9 @@
 class CampaignCreatedNotification
+  attr_reader :campaign, :musician
+
   def initialize(campaign)
     @campaign = campaign
+    @musician = campaign.musician
   end
 
   def deliver
@@ -9,13 +12,7 @@ class CampaignCreatedNotification
 
   private
 
-  attr_reader :campaign
-
-  def musician
-    campaign.musician
-  end
-
   def send_notification_to(musician)
-    UserMailer.campaign_created(musician, campaign)
+    UserMailer.campaign_created(musician, campaign).deliver
   end
 end
